b235b2814c7bc85777894c7be8ff3f360b42dd71
Hamza-Zafar2710 <159864278+Hamza-Zafar2710@users.noreply.github.com>
2026-07-15 14:21:33 +0500

    Setup project foundation with routing and layouts

--- jqz-homecare-admin/src/app/app.routes.ts
+++ jqz-homecare-admin/src/app/app.routes.ts
@@ -1,3 +1,44 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { AuthLayout } from './layouts/auth-layout/auth-layout';
+import { AdminLayout } from './layouts/admin-layout/admin-layout';
+
+import { Login } from './features/auth/login/login';
+import { Dashboard } from './features/dashboard/dashboard/dashboard';
+
+export const routes: Routes = [
+
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+
+  {
+    path: '',
+    component: AuthLayout,
+    children: [
+      {
+        path: 'login',
+        component: Login
+      }
+    ]
+  },
+
+  {
+    path: '',
+    component: AdminLayout,
+    children: [
+      {
+        path: 'dashboard',
+        component: Dashboard
+      }
+    ]
+  },
+
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
+
+];

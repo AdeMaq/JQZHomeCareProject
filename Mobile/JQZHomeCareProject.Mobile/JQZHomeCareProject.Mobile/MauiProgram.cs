7fae5e4faafe6f004c25048298f7e090af283d2f
Adeeba Maqbool <160094094+AdeMaq@users.noreply.github.com>
2026-08-25 17:35:11 +0500

    Added rest of the code for HomePage

--- Mobile/JQZHomeCareProject.Mobile/JQZHomeCareProject.Mobile/MauiProgram.cs
+++ Mobile/JQZHomeCareProject.Mobile/JQZHomeCareProject.Mobile/MauiProgram.cs
@@ -4,8 +4,10 @@ using JQZHomeCareProject.Mobile.Services.Api;
 using JQZHomeCareProject.Mobile.Services.Auth;
 using JQZHomeCareProject.Mobile.Services.Navigation;
 using JQZHomeCareProject.Mobile.ViewModels.Auth;
+using JQZHomeCareProject.Mobile.ViewModels.Home;
 using JQZHomeCareProject.Mobile.Views.Auth;
 using JQZHomeCareProject.Mobile.Views.Home;
+using JQZHomeCareProject.Mobile.Views.Visits;
 using Microsoft.Extensions.Logging;
 
 namespace JQZHomeCareProject.Mobile
@@ -35,11 +37,29 @@ namespace JQZHomeCareProject.Mobile
                 client.BaseAddress = new Uri(Constants.ApiBaseUrl);
             });
 
+            // --- Authenticated APIs ---
+            builder.Services.AddHttpClient<IVisitsApi, VisitsApi>(client =>
+            {
+                client.BaseAddress = new Uri(Constants.ApiBaseUrl);
+            }).AddHttpMessageHandler<AuthHeaderHandler>();
+
+            builder.Services.AddHttpClient<IRatingsApi, RatingsApi>(client =>
+            {
+                client.BaseAddress = new Uri(Constants.ApiBaseUrl);
+            }).AddHttpMessageHandler<AuthHeaderHandler>();
+
+            builder.Services.AddHttpClient<IPractitionersApi, PractitionersApi>(client =>
+            {
+                client.BaseAddress = new Uri(Constants.ApiBaseUrl);
+            }).AddHttpMessageHandler<AuthHeaderHandler>();
+
             // --- Pages / ViewModels ---
             builder.Services.AddTransient<SplashPage>();
             builder.Services.AddTransient<LoginPage>();
             builder.Services.AddTransient<LoginViewModel>();
             builder.Services.AddTransient<HomePage>();
+            builder.Services.AddTransient<HomeViewModel>();
+            builder.Services.AddTransient<VisitsPage>();
             builder.Services.AddTransient<AppShell>();
 
 
